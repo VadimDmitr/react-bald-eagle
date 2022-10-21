74eb718e6e4ad0614099e01cde00011c70c755dc
Vadim <vadzzimluch@gmail.com>
2022-10-21 14:53:15 -0400

    Added list items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react';
-
 function App() {
+  const todoList = [
+    { id: 1, title: "Read lesson" },
+    { id: 2, title: "Exercises" },
+    { id: 3, title: "Complete assigment" },
+  ];
+  
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+   <div className="App">
+     <h1>Todo List</h1>
+     <ul>
+     {todoList.map(function (item) {
+        return <li key={item.id}>{item.title}</li>;
+         })}
+     </ul>
+  </div>
+    );
+  }
 export default App;
+  //      return <li key={item.id}>{item.title}</li>;
+
+//Inside the JavaScript expression, map over your todoList array
+//For each Object in the Array, return a list item (<li>) with the following:
+//key attribute with value of id property
+//inner text content with value of title property
+//<ul>
+ //         {todoList.map(function (item) {
+ //           return <li>{item.title}</li>;
+ //       })}
+//
+ //      </ul>
+
+   //    return (
+   // <div style={{ textAlign: "center" }}>
+   //   <h1>Todo List</h1>
+   //   <ul>
+   //     {todoList.map(function (item) {
+    //      return <li key={item.id}>{item.title}</li>;
+     //   })}
+     // </ul>
+    //</div> 
