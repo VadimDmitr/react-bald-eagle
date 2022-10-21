1a0ba6c67145ea49b38b2783ed6a5ff600ed2f7d
Vadim <vadzzimluch@gmail.com>
2022-10-21 14:59:00 -0400

    Added list items

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     { id: 2, title: "Exercises" },
     { id: 3, title: "Complete assigment" },
   ];
-  
+
   return (
    <div className="App">
      <h1>Todo List</h1>
@@ -18,25 +18,4 @@ function App() {
     );
   }
 export default App;
-  //      return <li key={item.id}>{item.title}</li>;
-
-//Inside the JavaScript expression, map over your todoList array
-//For each Object in the Array, return a list item (<li>) with the following:
-//key attribute with value of id property
-//inner text content with value of title property
-//<ul>
- //         {todoList.map(function (item) {
- //           return <li>{item.title}</li>;
- //       })}
-//
- //      </ul>
-
-   //    return (
-   // <div style={{ textAlign: "center" }}>
-   //   <h1>Todo List</h1>
-   //   <ul>
-   //     {todoList.map(function (item) {
-    //      return <li key={item.id}>{item.title}</li>;
-     //   })}
-     // </ul>
-    //</div> 
+  
