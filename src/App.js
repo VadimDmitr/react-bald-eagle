64626016f7d92c6b8fd28273b5d99e6ce6920bfa
Vadim <vadzzimluch@gmail.com>
2023-02-03 13:48:36 -0500

    Added detail in CSS

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from "./Header";
-import Footer from "./Footer";
-
 
 const App = () => {
 
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = React.useState([null]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
@@ -35,84 +32,40 @@ const App = () => {
       localStorage.setItem('savedTodoList', JSON.stringify(todoList));
     }
     }, [todoList, isLoading]);
+  
+
 
     const removeTodo = (id) => {
-      // Make a DELETE request to Airtable API
-      fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default/${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            // Filter the todoList array to remove the deleted todo
-            const newTodoList = todoList.filter(
-              (todo) => todo.id !== id
-            );
-            setTodoList(newTodoList);
-          } else {
-            throw new Error('Failed to delete ToDo from Airtable');
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    }
-  
+    const newTodoList = todoList.filter(
+      (todo) => todo.id !== id
+    );
+    setTodoList(newTodoList);
+  };
   function addTodo(newTodo) {
-    const data = {
-      "fields": {
-      "Title": newTodo.title,
-      "Description": newTodo.description,
-      "Due Date": newTodo.dueDate,
-      "Status": newTodo.status
-      }
-    }
-  
-    // Make a POST request to Airtable API
-    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        setTodoList([...todoList, result]);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+
+    setTodoList([...todoList, newTodo]);
+
   }
      return (
       <BrowserRouter>
-        <Header />
               <Routes>
                 <Route exact path="/" element={
                   <div>
-                    <h1>ToDo List: </h1>
+                    <h1>Todo List: </h1>
                     <AddTodoForm onAddTodo={addTodo} />
                       {isLoading ? (
                         <p>Loading ...</p>
                         ) : (
-                          <TodoList 
-                          todoList={todoList} 
-                          onRemoveTodo={removeTodo} />
+                          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
                           )}
                   </div>
                 } />
                 <Route path="/new" element={
                   <div>
-                    <h1>New ToDo List: </h1>
+                    <h1>New Todo List: </h1>
                   </div>
                 } />
               </Routes>
-              <div>
-                <Footer />
-                </div>
           </BrowserRouter>
     );
 }
